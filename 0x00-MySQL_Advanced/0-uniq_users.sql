b0fd93996d18908ac84d278b5943c060c1f3aae0
ErastusMunala <munalaerastus@gmail.com>
2023-10-13 12:28:02 +0000

    0. We are all unique!

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,6 @@
+--creates a table userswith specific users.
+DROP TABLE IF EXISTS users;
+CREATE TABLE IF NOT EXISTS users (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+	);
